71469b1f808bc40cd6553c4eadef894d450d027a
Noah Lundquist <nalundquist@gmail.com>
2022-08-16 13:02:10 -0700

    add html output, fixes for formatting bugs in output
    Co-Authored-By: Mike Wilkes <mikewilkes@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
+// Utility Logic
+
+function isEmpty(testString) {
+  return (testString.trim().length === 0);
+}
+
 // Business Logic
 
 function wordCounter(text) {
-  if (text.trim().length === 0) {
+  if (isEmpty(text)) {
     return 0;
   }
   let wordCount = 0;
@@ -15,6 +21,9 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
+  if (isEmpty(text)) {
+    return 0;
+  }
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function(element) {
@@ -26,6 +35,9 @@ function numberOfOccurrencesInText(word, text) {
 }
 
 function naughtyWordDestroyer(text) {
+	if (isEmpty(text)){
+		return null;
+	}
 	const textArray = text.split(" ");
 	textArray.forEach(function(element) {
 		if (element.toLowerCase().includes("zoinks") || element.toLowerCase().includes("biffaroni") || element.toLowerCase().includes("loopdeloop") || element.toLowerCase().includes("muppeteer")) {
@@ -34,3 +46,41 @@ function naughtyWordDestroyer(text) {
 	console.log(element);
 	})
 }
+
+
+// UI Logic
+
+function boldPassage(word, text) {
+  if (isEmpty(word) || isEmpty(text)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
+
+function handleFormSubmission() {
+	event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
